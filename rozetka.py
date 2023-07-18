9fa6babc52c422ac275093e5831c5f2ed9517eff
unknown <k.ufimcev@yandex.ru>
2023-07-18 07:31:07 +0200

    rozetka

--- rozetka.py
+++ rozetka.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from config import *
 import database
 import cur_date
@@ -8,13 +9,20 @@ import time
 #getting the list of SKU urls
 def get_sku_urls(outer_url: str) -> list:
     
-    driver = webdriver.Chrome()
+    
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+    
     driver.maximize_window()
+    
     driver.get(outer_url)
     
     #geting number of pagination pages
     try:
-        pagen_len = int(driver.find_element(By.CLASS_NAME, 'nums').text.split()[-1])
+        pagen_len = int(driver.find_element(By.CLASS_NAME, 'pages').text.split()[-1])
     except:
         pagen_len = 1
     
@@ -22,15 +30,15 @@ def get_sku_urls(outer_url: str) -> list:
     
     #gathering the links of SKU's
     for i in range(1, pagen_len + 1):
-        driver.get(f'{outer_url}?PAGEN_1={i}')
-        links = driver.find_elements(By.XPATH, '//*[@id]/div/div[2]/div[1]/a')
+        driver.get(f'{outer_url}page-{i}/')
+        links = driver.find_elements(By.CLASS_NAME, 'item')
         for link in links:
-            if link.get_attribute('href') != 'https://baltmaximus.com/basket/':
-                sku_url.append(link.get_attribute('href'))
+            print(link.text)
+            sku_url.append(link.get_attribute('href'))
     
     for sku in sku_url:
         print(sku)
-    
+    """
     print(len(sku_url)) #qty of sku in category
     return sku_url
     
@@ -76,19 +84,19 @@ def get_page_data(urls: list) -> dict:
         database.write_to_database(data)
         
     return data
-
+    """
 def main():
 
     url_list = [
-        "https://baltmaximus.com/stiralnye_mashiny/",
-        "https://baltmaximus.com/paroochistiteli_i_otparivateli/",
-        "https://baltmaximus.com/monitory/",
-        "https://baltmaximus.com/smartfony/",
+        "https://www.rozetka39.ru/tehnika/krupnaya_bitovaya_tehnika/stiralnie_mashini/",
+        #"https://www.rozetka39.ru/tehnika/chistota_i_poryadok/otparivateli/",
+        #"https://www.rozetka39.ru/noutbuki_i_komputeri/monitori/",
+        #"https://www.rozetka39.ru/telefoni_i_plansheti/smartfoni_i_telefoni/tip_smartfon/",
     ]
     
     for url in url_list:
         
-        get_page_data(get_sku_urls(url))
+        get_sku_urls(url)
 
 if __name__ == '__main__':
     main()
